f15413a1bdbd52b58d93cf657891b9465df20f7e
sarah <sarah.laflamme@outlook.com>
2015-06-23 19:55:19 -0400

    Ajout des classes Panneau_centre et Panneau_centre_etudiant

--- src/Panneau_centre.java
+++ src/Panneau_centre.java
@@ -1,4 +1,15 @@
+import javax.swing.JLabel;
+import javax.swing.JTable;
+
 
 public class Panneau_centre extends Panneau {
 
+	JTable tableau_outils;
+	JTable tableau_bruts;
+	
+	public Panneau_centre()
+	{
+		
+	}
+	
 }
